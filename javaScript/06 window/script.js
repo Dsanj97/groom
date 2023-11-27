0b4449a7c2e8f5f745d36355669a9e0587a4344b
Dongseok Heo <dsanj97@gmail.com>
2023-11-27 12:22:41 +0900

    window

---
+++ javaScript/06 window/script.js
@@ -0,0 +1,21 @@
+let val
+
+val = window.outerHeight
+console.log(val)
+
+val = window.outerWidth
+console.log(val)
+
+val = window.innerHeight
+console.log(val)
+
+val = window.innerWidth
+console.log(val)
+
+val = window.scrollY
+console.log(val)
+
+val = window.location
+console.log(val)
+
+// window.location.href = 'http://google.com'
