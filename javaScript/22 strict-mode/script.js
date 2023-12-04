cb13275216d72f5c2eab6d8606b3de8d709d0de4
Dongseok Heo <dsanj97@gmail.com>
2023-12-04 16:24:43 +0900

    strict mode

---
+++ javaScript/22 strict-mode/script.js
@@ -0,0 +1,18 @@
+//'use strict'
+// 파일 전체에 적용하는것
+
+// 함수 내에서 전언 하는법
+function sum(a, b){
+    return a + b;
+}
+
+// class 를 사용하면 자동으로 strict가 된다
+class Cat {
+
+}
+
+let greeting = 'hello';
+
+greating = 'goodbye';
+
+console.log(greeting);
