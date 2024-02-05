5965b70164a5606b811da08fa616df2c0a84f8e1
Dongseok Heo <dsanj97@gmail.com>
2024-02-05 16:24:51 +0900

    order

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -10,8 +10,14 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
         // 회원 생성후 저장
         Long memberId = 1L;
@@ -21,7 +27,7 @@ public class OrderApp {
 
 
         // 주문 생성
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         // 주문 출력 --> toString() 으로 출력됨
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,9 +11,23 @@ public class OrderServiceImpl implements OrderService {
 
     // 두가지 기능이 필요함
     // 1. 맴버 저장소에서 회원 정보 가져오기
-    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+
     // 2. 할인정책 가져오기 (고정 할인 정책)
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 2. 할인정책 가져오기 (비율 할인 정책)
+    // 할인 정책이 바뀌면 코드도 바뀌어야하는 문제가 발생함
+
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     // 잘 구성된 설계의 에
     // 할인에대해서 잘 모르겠고 너가 알아서 해줘 라는뜻
