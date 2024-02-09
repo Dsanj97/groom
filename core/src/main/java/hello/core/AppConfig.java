6898eda164b387aff968e3566cc6a7d3c9f2f3e4
Dongseok Heo <dsanj97@gmail.com>
2024-02-09 15:31:23 +0900

    singleton container

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -12,29 +12,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+@Configuration // 싱글톤 보장을 위해 꼭 필요!!
 public class AppConfig {
+
+    // 싱글톤
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // 싱글톤아..?
+
+
+    //
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         // 밖에서 생성하여 넣어준다고 생각
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     // 할인정책 변경
     public DiscountPolicy discountPolicy() {
+        System.out.println("call AppConfig.discountPolicy");
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
