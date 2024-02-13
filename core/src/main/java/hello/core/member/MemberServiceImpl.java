534f21df99ce0694a07947b401b906bc0566e401
Dongseok Heo <dsanj97@gmail.com>
2024-02-13 11:40:22 +0900

    singleton container

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // 스피링 빈 으로 자동 등록
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //MemberRepository 타입에 맞는 빈을 자동으로 찾아 등록해준다
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
